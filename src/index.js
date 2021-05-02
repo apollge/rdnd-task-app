a8a9f334e8728c73933350109915464e112d81bc
Jan G <apollg3@gmail.com>
2021-05-02 20:31:33 +0800

    Persist List Reordering

--- src/index.js
+++ src/index.js
@@ -9,7 +9,41 @@ class App extends React.Component {
   state = initialData;
 
   onDragEnd = (result) => {
-    // @TODO reorder column
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    // Dragged to same position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+
+    // Move taskId from its old index to new index in the array
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
   };
 
   render() {
